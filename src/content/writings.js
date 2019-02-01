7a8a8757052f7cdb02cd11d26e3f5dbc85a7dc43
Adam Giese <adam.giese@underarmour.com>
2019-01-31 23:01:15 -0600

    Add initial Writings

---
+++ src/content/writings.js
@@ -0,0 +1,20 @@
+export default [
+  {
+    title: 'Level up your .filter game',
+    publication: 'CSS-Tricks',
+    link: 'https://css-tricks.com/level-up-your-filter-game/',
+    snippet: '.filter is a built-in array iteration method that accepts a predicate which is called against each of its values, and returns a subset of all values that return a truthy value.',
+  },
+  {
+    title: 'Writing for readability with declarative arrays',
+    publication: 'LogRocket Blog',
+    link: 'https://blog.logrocket.com/writing-for-readability-with-declarative-arrays-64f0b1a807f3',
+    snippet: 'By manipulating and digesting your arrays in a declarative fashion, you can produce much more readable code.',
+  },
+  {
+    title: 'Some Extremely Handy `:nth-child` Recipes as Sass Mixins',
+    publication: 'CSS-Tricks',
+    link: 'https://css-tricks.com/extremely-handy-nth-child-recipes-sass-mixins/',
+    snippet: 'Using :nth-child and :nth-last-child, you can get some surprisingly complex information without ever leaving your stylesheet. Here are some cool tricks that you can use to add some number-based logic to your CSS!',
+  },
+];
--- src/pages/writings.js
+++ src/pages/writings.js
@@ -1,8 +1,27 @@
 import React from 'react'
 import PageTitle from 'components/PageTitle'
+import writings from 'content/writings';
+import './writings.css'
+
+const LinkPost = ({title, link, snippet, publication}) => <article className='post post--link'>
+  <p className='postPublication'>{publication}</p>
+  <h2 className='postTitle'>{title}</h2>
+  <p className='postSnippet'>{snippet}</p>
+  <a className='postLink' href={link}>Read More</a>
+</article>
+
+const PostSwitch = props => {
+  if (props.link) {
+    return <LinkPost {...props} />
+  }
+  return null;
+}
 
 export default () => (
   <div>
     <PageTitle>Writings</PageTitle>
+    <div className='posts'>
+      { writings.map(post => <PostSwitch {...post} />) }
+    </div>
   </div>
 )
