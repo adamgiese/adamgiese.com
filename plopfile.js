2db1d42ac6a02e4fe40a05cf24da72eb92792af1
Adam Giese <adam.giese@underarmour.com>
2019-04-26 08:11:28 -0500

    Add speaking generator

--- plopfile.js
+++ plopfile.js
@@ -25,26 +25,38 @@ const prompts = {
     type: 'datetime',
     name: 'publicationDate',
     message: 'Publication Date: ',
-  }
+  },
+  eventName: {
+    type: 'input',
+    name: 'eventName',
+    message: 'Event Name: ',
+  },
+  eventDate: {
+    type: 'datetime',
+    name: 'eventDate',
+    message: 'Event Date: ',
+  },
 }
 
 module.exports = function (plop) {
   plop.setPrompt('datetime', datepickerPrompt);
   plop.setGenerator('writing', {
     description: 'A piece of writing, such as a blog post',
-    prompts: [
-      prompts.title,
-      prompts.publication,
-      prompts.publicationDate,
-      prompts.snippet,
-      prompts.link,
-    ],
-    actions: [
-      {
-        type: 'add',
-        path: 'content/writings/{{dashCase title}}.json',
-        templateFile: 'templates/writing.hbs',
-      },
-    ],
+    prompts: [ prompts.title, prompts.publication, prompts.publicationDate, prompts.snippet, prompts.link ],
+    actions: [ saveAsJson('writing', 'writings') ],
+  });
+
+  plop.setGenerator('speaking', {
+    description: 'A speaking gig, such as a conference or meetup',
+    prompts: [ prompts.title, prompts.link, prompts.eventName, prompts.eventDate, prompts.snippet ],
+    actions: [ saveAsJson('speaking', 'speakings') ],
   });
 };
+
+function saveAsJson(singular, plural) {
+  return {
+    type: 'add',
+    path: `content/${plural}/{{dashCase title}}.json`,
+    templateFile: `templates/${singular}.hbs`,
+  }
+}
